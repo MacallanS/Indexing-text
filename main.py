dae4154ac099d383cbd4982be99e1392d2f70208
xAsssenov <aks.kirill32@gmail.com>
2024-12-29 15:44:01 -0800

    fix: naming

--- main.py
+++ main.py
@@ -5,38 +5,36 @@ import tensorflow.keras as keras
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-X_train = X_train.reshape(-1, 28*28) / 255.0
-X_test = X_test.reshape(-1, 28*28) / 255.0
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+train_images = train_images.reshape(-1, 28*28) / 255.0
+test_images = test_images.reshape(-1, 28*28) / 255.0
+train_labels = np.array(train_labels)
+test_labels = np.array(test_labels)
 
-def plot_image(pixels: np.array):
-    plt.imshow(pixels.reshape((28, 28)), cmap='gray')
+def display_image(image_data: np.array):
+    plt.imshow(image_data.reshape((28, 28)), cmap='gray')
     plt.show()
 
-plot_image(X_train[25])
+display_image(train_images[25])
 
-y_train = y_train.reshape(-1, 1)
-encoder = OneHotEncoder(categories='auto')
-labels_onehot = encoder.fit_transform(y_train).toarray()
+train_labels = train_labels.reshape(-1, 1)
+label_encoder = OneHotEncoder(categories='auto')
+encoded_labels = label_encoder.fit_transform(train_labels).toarray()
 
-X_train, X_test, y_train, y_test = train_test_split(X_train, labels_onehot)
+train_images, test_images, train_labels, test_labels = train_test_split(train_images, encoded_labels)
 
-model = keras.Sequential([
+neural_net = keras.Sequential([
     keras.Input(shape=(28*28,)),
     keras.layers.Dense(units=128, activation='relu'),
     keras.layers.Dense(10, activation='softmax')
 ])
 
-model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+neural_net.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+neural_net.fit(train_images, train_labels, epochs=20, batch_size=128)
+neural_net.evaluate(test_images, test_labels)
 
-model.fit(X_train, y_train, epochs=20, batch_size=128)
+display_image(test_images[80])
 
-model.evaluate(X_test, y_test)
-
-plot_image(X_test[80])
-
-predicted_results = model.predict(X_test[80].reshape((1, -1)))
-print("Number on picture is", np.argmax(predicted_results))
+predicted_output = neural_net.predict(test_images[80].reshape((1, -1)))
+print("Number on picture is", np.argmax(predicted_output))
